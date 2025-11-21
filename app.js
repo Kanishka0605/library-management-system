d22ec624ad60d7bd830ffb0098c7159e73079546
Your Name <your-email@example.com>
2025-11-21 13:41:35 +0530

    Restructure for Render deployment: move frontend to /public, integrate MongoDB, update API routes

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
+require('dotenv').config();
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
+var mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var authRoutes = require('./server/routes/authRoutes');
+var bookRoutes = require('./server/routes/bookRoutes');
+var config = require('./server/config');
 
 var app = express();
+
+// Connect to MongoDB
+mongoose.connect(config.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Serve login page at root
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/login.html'));
+});
+
+// API routes
+app.use('/api', authRoutes);
+app.use('/api/books', bookRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
